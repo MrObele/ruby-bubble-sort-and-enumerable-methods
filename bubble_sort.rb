3bda4378536aae0d192c48a9f812355fec39f699
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2019-01-07 13:14:35 +0100

    added the bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,9 @@
-require 'profile'
-sample =[]
-    1000000000.downto(1).each do |x|
-    sample.push(x)
+# require 'profile'
+# sample =[]
+#     1000000000.downto(1).each do |x|
+#     sample.push(x)
 
-    end
+#     end
 
 def bubble_sort(input_array)
   
@@ -27,4 +27,21 @@ def bubble_sort(input_array)
 end
 
 
-bubble_sort sample
+def bubble_sort_by(input_array) 
+  finish_point = input_array.length - 2
+   
+    (input_array.length - 1).times do
+      for i in 0.upto(finish_point) do
+
+        if yield(input_array[i], input_array[i + 1]) < 0
+          input_array[i], input_array[i + 1] = input_array[i + 1], input_array[i]
+        end
+      
+      end
+      finish_point -= 1
+
+    end
+
+    puts input_array
+end
+# bubble_sort sample
